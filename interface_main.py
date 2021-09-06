c83302e8154c23e27f912ba58078168a0554a2fb
Your Name <mike.liang839@gmail.com>
2021-09-06 17:01:22 +1000

    FINNALY I CAN DISPLAY A BOARD

--- interface_main.py
+++ interface_main.py
@@ -1,8 +1,8 @@
 import engine
 import pygame as p
 import os
-
-os.environ["SDL_VIDEODRIVER"] = "dummy"
+os.environ["SDL_VIDEODRIVER"]="x11"
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
 
 p.init()
 WIDTH = HEIGHT = 512
@@ -24,10 +24,33 @@ def main():
     screen.fill(p.Color("white"))
     gs = engine.gameState()
     load_images()
-    
-    
+    running = True
+    while running:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+        
+        draw_game_state(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
-main()
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    
+    draw_pieces(screen, gs.board)
+
+def draw_board(screen):
+    colors = [p.Color("white"), p.Color("pink")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) %2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE,SQ_SIZE))
+    
+def draw_pieces(screen, board):
+    pass
+
+if __name__ == '__main__':
+    main()
 
 
 
